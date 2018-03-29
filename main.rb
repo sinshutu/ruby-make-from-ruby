ddc93161b4efdf28c1fdfdf4feac4ea198960fb7
NaotoSuzuki <c0114292a2@edu.teu.ac.jp>
2018-03-29 17:23:39 +0900

    変数の処理を実装

--- main.rb
+++ main.rb
@@ -6,6 +6,7 @@ def evoluate(tree, env)
   when "lit"
     tree[1]
   when "+"
+    env['plus_count'] += 1 unless env['plus_count'].nil?
     evoluate(tree[1], env) + evoluate(tree[2], env)
   when "-"
     evoluate(tree[1], env) - evoluate(tree[2], env)
@@ -25,6 +26,10 @@ def evoluate(tree, env)
     evoluate(tree[1], env) < evoluate(tree[2], env)
   when "func_call"
     p(evoluate(tree[2], env))
+  when "var_assign"
+    env[tree[1]] = evoluate(tree[2], env)
+  when "var_ref"
+    env[tree[1]]
   when "stmts"
     i = 1
     while tree[i] != nil
@@ -34,6 +39,19 @@ def evoluate(tree, env)
     last
   else
     "no support operand: #{tree[0]}"
+  end
+end
+
+def plus_count(tree)
+  case tree[0]
+  when "lit"
+    tree[1]
+  else
+    if max(tree[1]) < max(tree[2])
+      max(tree[2])
+    else
+      max(tree[1])
+    end
   end
 end
 
